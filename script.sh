09da27e409b801ad4321dcad431e86c603384c4d
denilb <43617464+denilb@users.noreply.github.com>
2022-03-30 16:39:06 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -23,5 +23,3 @@ sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams
 #Brave
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
-# Installing Chrome Browswer
