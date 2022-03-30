90c0d9100843ec3f594ef4de7bbc234e4c458821
denilb <43617464+denilb@users.noreply.github.com>
2022-03-30 12:14:18 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,13 @@
 #!/bin/bash/
 
+#Unintsalling 
+sudo apt-get --purge remove libreoffice* -y
+sudo apt-get --purge remove gnome-calendar -y
+sudo apt-get --purge remove gnome-contacts -y
+sudo apt-get --purge remove geary -y
+sudo apt-get --purge remove gnome-weather -y
+
+
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
