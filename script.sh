4d425eb9c4c2ff967a7c958d8e99a3aa5d973c2a
Perseus-101 <43617464+Perseus-101@users.noreply.github.com>
2023-01-11 01:11:13 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,18 +16,7 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 
 # Installing applications, included in the apt package manager
-sudo apt-get install vlc telegram-desktop code qbittorrent discord google-chrome-stable apt-transport-https curl -y
-
-#Teams
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
-#Brave
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt-get install vlc telegram-desktop code qbittorrent google-chrome-stable apt-transport-https curl -y
 
 #Updating all the repositories 
 sudo apt update
-
-#Installing additional applications
-sudo apt-get install teams
-sudo apt-get install brave
